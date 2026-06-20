d2eec7216e691799bff7f3a3281a51128317f410
seamless45-cpu <sixsevensixseven0000000@gmail.com>
2026-06-20 14:24:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5202,7 +5202,7 @@ async function mainScript(initialBaseTex, initialWaterTex, initialWallTex, initi
   const colorScalesTexture = gl.createTexture();
 
   const lightningTextures = [];
-  const numLightningTextures = 3;
+  const numLightningTextures = 12;
 
 
   frameBuff_0 = gl.createFramebuffer(); // global for weather stations
